1b7c4b0c7925e6f36a07b791924bcae2d778a441
Ram Anam <ram.anam@totalsynergy.com>
2018-01-12 14:07:37 +1100

    First version

---
+++ src/actions/index.jsx
@@ -0,0 +1,2 @@
+
+function selectBook(book){}
---
+++ src/reducers/reducers_books.js
@@ -0,0 +1,8 @@
+//Reducer is a part of the entire state.
+export default function (){
+	return [
+		{title: "Javascripts : Good parts" },
+		{title: "Harry Potter"},
+		{title: "The dark tower"}
+	]
+}
